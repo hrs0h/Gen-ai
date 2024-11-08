79759a939965280e1676e3ba56e66fcd59fe37c0
Harsh <harsh5656kumar5656@gmail.com>
2024-11-08 09:11:54 +0530

    changes in working iwht gemini. py file

--- workingWithGemini.py
+++ workingWithGemini.py
@@ -1,4 +1,13 @@
-from dotenv import load_dotenv 
-import os 
-load_dotenv() 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+load_dotenv()
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key="AIzaSyDJYeOm2DcAgHAXvmMN8N5l-XlLrBfDwOY")
+prompt_template = PromptTemplate.from_template("Write a tweet about {topic}.")
+chain = llm | prompt_template
+response = chain.invoke("AI in healthcare")
+
+print(response.to_messages())
